fa6a03d6238653764bfaa3b90d39781c4c0c5c75
NyusDepresi <NyusDepresi@gmail.com>
2015-08-30 21:16:25 +0800

    Create NYUS-Readmore.js

---
+++ NYUS-Readmore.js
@@ -0,0 +1,6 @@
+<script type='text/javascript'>
+summary_noimg = 530; /* jumlah teks tanpa thumbnail */
+summary_img = 440;  /* Jumlah teks dengan auto thumbnail */
+img_thumb_height =&#39;auto&#39;;
+img_thumb_width = 100; /* width thumbnail * Semakin besar akan semakin lebar dan tinggi */
+</script>
