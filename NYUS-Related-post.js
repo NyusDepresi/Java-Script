b094e1f2a03d2618cd73b08e2b77a1a00bc6833b
NyusDepresi <NyusDepresi@gmail.com>
2015-08-30 21:09:39 +0800

    Create NYUS-Related-post.js

---
+++ NYUS-Related-post.js
@@ -0,0 +1,16 @@
+//<![CDATA[
+eval(function(p,a,c,k,e,r){e=function(c){return(c<a?'':e(parseInt(c/a)))+
+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String))
+{while(c--)r[e(c)]=k[c]||e(c);k=[function(e){return r[e]}];e=function(){return'\\w+'};c=1};while(c--)
+if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p}('3 4=8 9();3 e=0;3 5=8 9();f s(c)
+{7(3 b=0;b<c.k.l.2;b++){3 d=c.k.l[b];4[e]=d.u.$t;7(3 a=0;a<d.h.2;a++){6(d.h[a].v=="w")
+{5[e]=d.h[a].m;e++;x}}}}f y(){3 a=8 9(0);3 b=8 9(0);7(3 c=0;c<5.2;c++){6(!n(a,5[c]))
+{a.2+=1;a[a.2-1]=5[c];b.2+=1;b[b.2-1]=4[c]}}4=b;5=a}f n(b,d){7(3 c=0;c<b.2;c++)
+{6(b[c]==d){o z}}o A}f B(c){7(3 a=0;a<5.2;a++){6(5[a]==c){5.g(a,1);4.g(a,1)}}3 b=p.C((4.2-1)*p.D());3 a=0;6(4.2>1)
+{i.j("<q>")}E(a<4.2&&a<F&&a<G){i.j(\'<r><a m="\'+5[b]+\'">\'+4[b]+"</a></r>");6(b<4.2-1)
+{b++}H{b=0}a++}i.j("</q>");5.g(0,5.2);4.g(0,4.2)}',44,44,'
+||length|var|relatedTitles|relatedUrls|if|for|new|Array|||||
+relatedTitlesNum|function|splice|link|document|write|feed|entry|href|contains|return|Math|ul|li|
+related_results_labels||title|rel|alternate|break|removeRelatedDuplicates|true|false|
+printRelatedLabels|floor|random|while|20|maxresults|else'.split('|'),0,{})) //]]> 
+/* NYUS_related-posts.js * http://nyusblog.blogspot.com */
